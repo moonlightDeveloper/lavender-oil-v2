c1c2018f7276799b35a6de53759164c037f085f9
magdalena.ilieva <magdalena.ilieva@accedia.com>
2019-07-20 20:00:46 +0300

    adding natural icon

--- src/components/home/Home-Header.js
+++ src/components/home/Home-Header.js
@@ -18,7 +18,7 @@ const HomeHeader =() =>  {
                                             Bulgaria</h3>
                                         <br/> 
                                         <div className="natural-img-container">                                     
-                                            <img src="/natural.png" className="natural-img" alt="not found"/>
+                                            <img src="/natural.svg" className="natural-img" alt="not found"/>
                                         </div>
                                         <br/>
                                         <a className="btn btn-md animation bounce-in" href="#services">Learn
@@ -29,6 +29,7 @@ const HomeHeader =() =>  {
                         </div>
                     </div>
                 </div>
+                <img src="/natural.svg" className="natural-img-mobile" alt="not found" />
             </section>
         );
 }
