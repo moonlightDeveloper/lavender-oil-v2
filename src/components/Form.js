cac21b3b7bfbf305f87294bec4d509765b14f8a5
George Naydenov <georgi.naidenovelg@gmail.com>
2019-07-28 22:48:33 +0300

    fdfsdfsdf

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,6 +11,8 @@ const Form = props => {
                 <input name="userTelephone" className="gutter" type="text" placeholder="Telephone" value={userTelephone} onChange={handleChange} />
                 <input type="text" placeholder="Email" value={userMail} name="userMail" onChange={handleChange} />
                 <textarea name="feedback" id="tx" placeholder="Leave a message" value={feedback} onChange={handleChange}></textarea>
+                <div class="g-recaptcha" data-sitekey="6LcM768UAAAAALhJLZjbcApG7UzZuoft9kArifqP"></div>
+                <br/>
                 <input type="submit" value="Send" />
             </form>
 };
