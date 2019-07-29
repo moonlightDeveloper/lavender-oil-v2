705afff3946e57680a468d4d0cd92df079dd3b2b
magdalena.ilieva <magdalena.ilieva@accedia.com>
2019-07-29 18:44:35 +0300

    hash tag removed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {HashRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './i18n';
 import App from './App';
 import './assets/stylesheets/App.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render((
-    <HashRouter>
+    <BrowserRouter>
         <App />
-    </HashRouter>),
+    </BrowserRouter>),
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
